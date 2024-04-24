8b91b05075b0d7a5deb2297025a8fbbb667ec806
Matt Jones <me@mattjones.tech>
2024-04-23 21:53:15 -0400

    add next challenge

--- string-function-excercises.sql
+++ string-function-excercises.sql
@@ -1,4 +1,13 @@
 -- String function exerceises
 
--- reverse and uppercase the following string:
+-- reverse and uppercase the following string (single query):
 -- "Why does my cat look at me with such hatred?"
+
+-- What does the following evaluate to?
+SELECT
+	REPLACE
+    (
+    CONCAT('I', ' ', 'like', ' ', 'cats'),
+    ' ',
+    '-'
+    );
