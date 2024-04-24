c9309b38d68ca8b9461217563549e347a63e8f8e
Matt Jones <me@mattjones.tech>
2024-04-24 05:51:14 -0400

    finish writing instructions for all exerciess

--- string-function-excercises.sql
+++ string-function-excercises.sql
@@ -11,3 +11,16 @@ SELECT
     ' ',
     '-'
     );
+    
+-- Write a query using the books table that replaces spaces in titles with '->' and name the column title
+
+-- Write a query outputing 2 columns. 1st named 'forwards' with author's first name and 2nd named 'backwards' with author's first name printed backwards
+
+-- Write a query resulting in one table named 'full name in caps' that outputs authors full name in all caps
+
+-- Write a query resulting in one table named 'blurb' that combines book title with 'was released in' and the release year.
+
+-- Print book titles and the length of each book title (two columns named 'title' and 'character count' respectively
+
+-- FINAL BOSS
+-- Output 3 columns. First named 'short title' that outputs the first 10 characters of a book title, truncated by '...'. The next column named 'author' outputs auther's lastname, comma, first name. The final column named 'quantity' outputs the quantity and 'in stock'.
