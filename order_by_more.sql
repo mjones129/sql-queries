46a7a7d981dac927adbf185b548eadd23af755c3
Matt Jones <me@mattjones.tech>
2024-04-25 17:04:49 -0400

    order by a custom alias

--- order_by_more.sql
+++ order_by_more.sql
@@ -3,3 +3,6 @@ SELECT book_id, author_fname, pages FROM books ORDER BY 3;
 
 -- order by multiple columns (sort by author's last name, and for each result under that last name, sort by released_year.
 SELECT author_lname, released_year, title FROM books ORDER BY author_lname, released_year;
+
+-- concating multiple results into a new table under an alias 'author', and then sorting by that alias.
+SELECT CONCAT(author_fname, ' ', author_lname) AS author FROM books ORDER BY author;
