d0bef0b8688278c2af56371b889d2db50da32b06
Matt Jones <me@mattjones.tech>
2024-04-25 21:27:58 -0400

    escaping underscores

--- escaping_wildcards.sql
+++ escaping_wildcards.sql
@@ -5,3 +5,6 @@ SELECT * FROM books WHERE title LIKE '%';
 
 -- This is a literal percent sign within wildcards and returns the only title containing a literal percent sign
 SELECT * FROM books WHERE title LIKE '%\%%';
+
+-- Escaping a '_' works the same way
+SELECT * FROM books WHERE title LIKE '%\_%';
