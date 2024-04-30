13de11d3083cd8417b8c2cfa560596ff348fe5e3
Matt Jones <me@mattjones.tech>
2024-04-30 06:30:31 -0400

    finish count

--- count.sql
+++ count.sql
@@ -2,4 +2,16 @@
 SELECT COUNT(*) FROM books;
 
 -- How namy author first names are in the database?
-SELECT COUNT(*) FROM books;
+SELECT COUNT(author_fname) FROM books;
+
+-- Both the above functions return the same number. Add two blank entries to differentiate (run twice)
+INSERT INTO books () VALUES ();
+
+-- How many DISTINCT author first names?
+SELECT COUNT(DISTINCT(author_fname)) FROM books;
+
+-- Count the unique last names
+SELECT COUNT(DISTINCT(author_lname)) FROM books;
+
+-- How many titles contain "the"?
+SELECT title FROM books WHERE COUNT(title LIKE "%the%");
