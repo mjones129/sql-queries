b4339f3a195ae0e8ff8bdfadb3668d7160a5c658
Matt Jones <me@mattjones.tech>
2024-05-11 07:34:42 -0400

    finish group_by

--- group_by.sql
+++ group_by.sql
@@ -1,4 +1,10 @@
 -- Group by
 SELECT author_lname FROM books GROUP BY author_lname;
 
+-- count the number of instances of each author_lname
 SELECT author_lname, COUNT(*) FROM books GROUP BY author_lname;
+
+-- total number of books written by each author
+SELECT author_lname, COUNT(*) AS books_written FROM books GROUP BY author_lname ORDER BY books_written DESC;
+
+SELECT released_year, COUNT(*) FROM books GROUP BY released_year
