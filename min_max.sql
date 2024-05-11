233f21dd48cfa49a783cf1b2f5ea3f228d44f59f
Matt Jones <me@mattjones.tech>
2024-05-11 07:41:39 -0400

    min and max

---
+++ min_max.sql
@@ -0,0 +1,7 @@
+-- select minimum release year
+SELECT MIN(released_year) FROM books;
+
+-- find the book with the most number of pages
+SELECT MAX(pages) FROM books;
+
+SELECT MIN(author_lname) FROM books;
