2fd73188389bd7b5fe7c50d1e288747772d7514a
Matt Jones <me@mattjones.tech>
2024-06-05 20:29:23 -0400

    intro to date, time and datetime

---
+++ dates.sql
@@ -0,0 +1,13 @@
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DpeopleATETIME );
+
+
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Elton', '2000-12-25', '11:00:00', '2000-12-25 11:00:00');
+
+SELECT * FROM people;
+
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Lulu', '1985-04-11', '09:45:10', '1985-04-11 11:00:00');
+
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Juan', '2020-08-15', '23:59:00', '2020-08-15 23:59:00');
