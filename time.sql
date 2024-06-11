c24b69ff92b2b2ed3d5308c1053b3858f7e3c7e9
Matt Jones <me@mattjones.tech>
2024-06-10 21:02:35 -0400

    formatting dates and times

--- time.sql
+++ time.sql
@@ -7,3 +7,11 @@ SELECT name, birthtime, SECOND(birthtime) FROM people;
 SELECT birthdt, HOUR(birthdt) FROM people;
 
 SELECT birthdt, DATE(birthdt), TIME(birthdt) FROM people;
+
+
+SELECT MONTHNAME(birthdate), DAY(birthdate), YEAR(birthdate) FROM people;
+
+-- display birth date in a specific format
+SELECT birthdate, DATE_FORMAT(birthdate, '%a %b %D') FROM people;
+
+SELECT birthdt, DATE_FORMAT(birthdt, '%r') FROM people;
