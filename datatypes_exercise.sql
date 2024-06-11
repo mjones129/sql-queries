0a6e3f7bab48368a0e79b1590f11e939447f1a8c
Matt Jones <me@mattjones.tech>
2024-06-11 06:48:41 -0400

    finish datatypes exercise

--- datatypes_exercise.sql
+++ datatypes_exercise.sql
@@ -21,3 +21,16 @@ SELECT DAYOFWEEK(CURDATE());
 
 -- print out the day of the week (name)
 SELECT DAYNAME(CURDATE());
+
+-- Print out the current day in the following format: mm/dd/yyyy
+SELECT DATE_FORMAT(CURDATE(), '%m/%d/%Y');
+
+-- print current day and time using this format: January 2nd at 3:15 (24hr)
+SELECT DATE_FORMAT(NOW(), '%M %D at %k:%i');
+
+-- create a tweets table that stores: tweet content, username, time it was created
+CREATE TABLE tweets (
+	content VARCHAR(180),
+    username VARCHAR(20),
+    created_at TIMESTAMP DEFAULT NOW()
+);
