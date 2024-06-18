6d4da64b42efc35a5e74fdec9fb27ef095d76802
Matt Jones <me@mattjones.tech>
2024-06-18 16:11:45 -0400

    alter table

---
+++ alter_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO companies (name, address)
+VALUES ('blackbird auto', '123 spruce');
+
+INSERT INTO companies (name, address)
+VALUES ('luigis pies', '123 spruce');
+
+SELECT * FROM companies;
