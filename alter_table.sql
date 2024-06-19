fec2ff085f308b47a567d650ea8f26a752c137ce
Matt Jones <me@mattjones.tech>
2024-06-18 20:54:58 -0400

    drop columns

--- alter_table.sql
+++ alter_table.sql
@@ -5,3 +5,10 @@ INSERT INTO companies (name, address)
 VALUES ('luigis pies', '123 spruce');
 
 SELECT * FROM companies;
+
+-- drop a column
+ALTER TABLE companies
+DROP COLUMN phone; -- null values
+
+ALTER TABLE companies
+DROP COLUMN employee_count; -- contains values, but MYSQL don't care
