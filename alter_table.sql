2f5340ce6ea95fe78a9e24f75c0cd13ec1706d8d
Matt Jones <me@mattjones.tech>
2024-06-18 21:07:31 -0400

    renaming tables and colunns

--- alter_table.sql
+++ alter_table.sql
@@ -12,3 +12,12 @@ DROP COLUMN phone; -- null values
 
 ALTER TABLE companies
 DROP COLUMN employee_count; -- contains values, but MYSQL don't care
+
+-- rename tables
+RENAME TABLE companies TO suppliers;
+
+-- alternative renaming syntax
+ALTER TABLE suppliers RENAME TO companies;
+
+-- renaming columns
+ALTER TABLE companies RENAME COLUMN name TO company_name;
