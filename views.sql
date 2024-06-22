fe46c2691ce444968cc33a459737cfead6063345
Matt Jones <me@mattjones.tech>
2024-06-22 18:05:07 -0400

    finish modes

--- views.sql
+++ views.sql
@@ -70,3 +70,6 @@ SELECT @@GLOBAL.sql_mode;
 
 -- to modify modes, provide a list of settings as a string
 SET GLOBAL sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- strict trans(actional) tables
+
