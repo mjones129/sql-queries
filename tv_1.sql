1b6a27e4a143cb5d53cead2f580378872dafb711
Matt Jones <me@mattjones.tech>
2024-06-21 22:34:47 -0400

    tv challenge 2

--- tv_1.sql
+++ tv_1.sql
@@ -1,2 +1,9 @@
 SELECT title, rating FROM series
 JOIN reviews ON series.id = reviews.series_id;
+
+-- tv challenge 2
+SELECT title, AVG(rating) as avg_rating
+FROM series
+JOIN reviews ON series.id = reviews.series_id
+GROUP BY title
+ORDER BY avg_rating;
