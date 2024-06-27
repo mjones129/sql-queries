25ebc76cab9190223c4c65dc9a956f3b499c76e2
Matt Jones <me@mattjones.tech>
2024-06-27 15:51:36 -0400

    add likes table

--- ig1.sql
+++ ig1.sql
@@ -27,3 +27,13 @@ CREATE TABLE comments (
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+-- create likes table
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
