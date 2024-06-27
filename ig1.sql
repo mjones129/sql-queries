28a1784726992c0acdd890d7f31414b78e925d3b
Matt Jones <me@mattjones.tech>
2024-06-27 16:03:58 -0400

    add followers table

--- ig1.sql
+++ ig1.sql
@@ -37,3 +37,13 @@ CREATE TABLE likes (
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     PRIMARY KEY(user_id, photo_id)
 );
+
+-- create followers table
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
