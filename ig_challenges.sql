4789381cb0bc6d933a8cf7e27b56a00a32d98054
Matt Jones <me@mattjones.tech>
2024-06-28 12:56:14 -0400

    ig challenge 4

--- ig_challenges.sql
+++ ig_challenges.sql
@@ -15,3 +15,16 @@ SELECT COUNT(*) AS total, DAYNAME(created_at) AS day FROM users GROUP BY day ORD
 SELECT username FROM users
 LEFT JOIN photos ON users.id = photos.user_id
 WHERE photos.id IS NULL;
+
+-- we're running a new contest to see who can get the most likes on a single photo. WHO WON???
+SELECT username, COUNT(*) AS total_likes FROM users
+LEFT JOIN likes ON users.id = likes.user_id GROUP BY username ORDER BY total_likes DESC;
+-- returns total likes per user :/
+
+-- solution
+SELECT photos.id, photos.image_url, COUNT(*)
+FROM photos
+INNER JOIN likes
+	ON likes.photo_id = photos.id
+    GROUP BY photos.id
+    ORDER BY COUNT(*) DESC;
