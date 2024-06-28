dfe0b1a41f027ce0d39ed3fcb2a5d9d2c5b60d63
Matt Jones <me@mattjones.tech>
2024-06-28 12:38:11 -0400

    ig challenge 3

--- ig_challenges.sql
+++ ig_challenges.sql
@@ -10,3 +10,8 @@ Select AVG(DAYOFWEEK(created_at)) FROM users;
 
 SELECT COUNT(*) AS total, DAYNAME(created_at) AS day FROM users GROUP BY day ORDER BY total DESC;
 -- Thurs and Sun both have 16 registrations
+
+-- find the users who have never posted a photo
+SELECT username FROM users
+LEFT JOIN photos ON users.id = photos.user_id
+WHERE photos.id IS NULL;
